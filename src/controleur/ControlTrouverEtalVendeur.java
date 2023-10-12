d08046c4579c85b534456d0758d727cc57a60aa7
Matis Essengue <85627723+matis-essengue@users.noreply.github.com>
2023-10-12 09:35:19 +0200

    TP2-5-TrouverEtalVide

--- src/controleur/ControlTrouverEtalVendeur.java
+++ src/controleur/ControlTrouverEtalVendeur.java
@@ -1,5 +1,6 @@
 package controleur;
 
+import personnages.Gaulois;
 import villagegaulois.Etal;
 import villagegaulois.Village;
 
@@ -11,8 +12,10 @@ public class ControlTrouverEtalVendeur {
 	}
 
 	public Etal trouverEtalVendeur(String nomVendeur) {
-		//TODO a completer
-		Etal etal = null;
-		return etal;
+		Gaulois gaulois = village.trouverHabitant(nomVendeur);
+		if (gaulois != null) {
+			return village.rechercherEtal(gaulois);
+		}
+		return null;
 	}
 }
